3d754967c335ae62af71fcf111e2e9066d403705
Summersaltz <mcrandle2002@gmail.com>
2017-07-14 11:53:18 -0400

    Add files via upload

---
+++ Pygame (2).py
@@ -0,0 +1,129 @@
+#Pygame
+import pygame
+import random
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#define colours (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player(pygame.sprite.Sprite):
+    #sprite for player
+    def __init__(self): #initializes sprite
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((45,45))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = (WIDTH/2)
+        self.rect.bottom = HEIGHT - 15
+        
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        elif keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        elif self.rect.left < 0:
+            self.rect.left = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+        
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25,35))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(2, 10)
+        self.speedx = random.randrange(-2, 2)
+        
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.right < 0 or self.rect.left > WIDTH: 
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(2, 10)
+            self.speedx = random.randrange(-2, 2)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+        
+#initialize pygame and create our window
+pygame.init()
+pygame.mixer.init() #enalbles sound
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+mobs = pygame.sprite.Group()
+for i in range(8):
+    mob = Mob()
+    all_sprites.add(mob)
+    mobs.add(mob)
+bullets = pygame.sprite.Group()
+
+#Game loop
+running = True
+while running:
+    #keep loop running at the right speed
+    clock.tick(FPS)
+    #process input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+    #update
+    all_sprites.update()
+    
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        mob = Mob()
+        all_sprites.add(mob)
+        mobs.add(mob)
+        
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+    #draw/render
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    #after drawing everyting, flip the display
+    pygame.display.flip()
+pygame.quit()
+    
+
